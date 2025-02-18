4bda49b771a6e157e205c71bb3f9b9728d1496a6
ahmadbilalfazal <147061947+ahmadbilalfazal@users.noreply.github.com>
2025-02-19 01:05:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ document.querySelectorAll('.feature').forEach(feature => {
         feature.style.boxShadow = 'none';
     });
 });
+
+document.getElementById('notify-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Thanks for subscribing! We\'ll notify you when we launch.');
+            form.reset();
+        } else {
+            alert('Oops! Something went wrong. Please try again.');
+        }
+    })
+    .catch(error => {
+        alert('Oops! Something went wrong. Please try again.');
+    });
+});
